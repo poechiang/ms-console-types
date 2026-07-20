1edfa58b9ed61992ffd0b580214893d1f2d32ea8
poechiang <poechiang@live.cn>
2026-07-21 07:01:14 +0800

    fix: format

--- modules/frame/FrameHeaderProps.ts
+++ modules/frame/FrameHeaderProps.ts
@@ -1,19 +1,19 @@
-import type { HeaderMenuItem, ThemeMode } from '../../common/global';
-import type { LocaleKeys } from '../../common/LocaleKeys';
+import type { HeaderMenuItem, ThemeMode } from '../../common/global'
+import type { LocaleKeys } from '../../common/LocaleKeys'
 
 export interface MacroService {
-  title: string;
-  logo?: string;
-  description?: string;
-  link: string;
+  title: string
+  logo?: string
+  description?: string
+  link: string
 }
 
 export interface UseHeaderStoreReturn {
-  title: string;
-  services: MacroService[];
-  menuItems: HeaderMenuItem[];
-  selectedMenuKey: string;
-  locale: LocaleKeys;
-  theme: ThemeMode;
-  coloring: string;
+  title: string
+  services: MacroService[]
+  menuItems: HeaderMenuItem[]
+  selectedMenuKey: string
+  locale: LocaleKeys
+  theme: Omit<ThemeMode, 'auto'>
+  coloring: string
 }
